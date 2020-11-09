f5e73ae5d0742c4d1a685c3742105a5cce0b5692
Bami <bbamii@outlook.com>
2020-11-09 05:17:31 +0100

    done...?

--- javascript/script.js
+++ javascript/script.js
@@ -86,22 +86,22 @@ $(document).ready(function () {
     );
   });
 
-  $(".dropdown").hover(
-    function () {
-      $(".dropdown-menu", this)
-        .not(".in .dropdown-menu")
-        .stop(true, true)
-        .slideDown("400");
-      $(this).toggleClass("open");
-    },
-    function () {
-      $(".dropdown-menu", this)
-        .not(".in .dropdown-menu")
-        .stop(true, true)
-        .slideUp("400");
-      $(this).toggleClass("open");
-    }
-  );
+  // $(".dropdown").hover(
+  //   function () {
+  //     $(".dropdown-menu", this)
+  //       .not(".in .dropdown-menu")
+  //       .stop(true, true)
+  //       .slideDown("400");
+  //     $(this).toggleClass("open");
+  //   },
+  //   function () {
+  //     $(".dropdown-menu", this)
+  //       .not(".in .dropdown-menu")
+  //       .stop(true, true)
+  //       .slideUp("400");
+  //     $(this).toggleClass("open");
+  //   }
+  // );
 
   var speed = 500;
   var autoswitch = true;
@@ -113,43 +113,43 @@ $(document).ready(function () {
 
   var $call = $(".call");
 
-  $(".down").on("click", next);
-  $(".down").on("click", main);
-  function next() {
-    $counter++;
-    if ($counter >= txtArray.length) $counter = 0;
-    $(".caption_text").hide();
-    $.each(txtArray, function () {
-      $new_ctn++;
-      if ($new_ctn >= txtArray.length) $new_ctn = 0;
+  // $(".down").on("click", next);
+  // $(".down").on("click", main);
+  // function next() {
+  //   $counter++;
+  //   if ($counter >= txtArray.length) $counter = 0;
+  //   $(".caption_text").hide();
+  //   $.each(txtArray, function () {
+  //     $new_ctn++;
+  //     if ($new_ctn >= txtArray.length) $new_ctn = 0;
 
-      if ($counter === $new_ctn) {
-        console.log("Count is " + $counter + " and new count is : " + $new_ctn);
-        $(txtArray[$new_ctn]).fadeIn(500);
-      }
-    });
-  }
-  $(".up").on("click", function () {
-    $counter--;
-    console.log($counter);
-    if ($counter < 0) $counter = txtArray.length - 1;
+  //     if ($counter === $new_ctn) {
+  //       console.log("Count is " + $counter + " and new count is : " + $new_ctn);
+  //       $(txtArray[$new_ctn]).fadeIn(500);
+  //     }
+  //   });
+  // }
+  // $(".up").on("click", function () {
+  //   $counter--;
+  //   console.log($counter);
+  //   if ($counter < 0) $counter = txtArray.length - 1;
 
-    $(".caption_text").hide();
-    $.each(txtArray, function () {
-      $new_ctn--;
-      if ($new_ctn < 0) $new_ctn = txtArray.length - 1;
+  //   $(".caption_text").hide();
+  //   $.each(txtArray, function () {
+  //     $new_ctn--;
+  //     if ($new_ctn < 0) $new_ctn = txtArray.length - 1;
 
-      if ($counter === $new_ctn) {
-        console.log("Count is " + $counter + " and new count is : " + $new_ctn);
+  //     if ($counter === $new_ctn) {
+  //       console.log("Count is " + $counter + " and new count is : " + $new_ctn);
 
-        $(txtArray[$new_ctn]).fadeIn(500);
-      }
-    });
-  });
+  //       $(txtArray[$new_ctn]).fadeIn(500);
+  //     }
+  //   });
+  // });
 
-  if (autoswitch == true) {
-    setInterval(next, autoswicth_speed);
-  }
+  // if (autoswitch == true) {
+  //   setInterval(next, autoswicth_speed);
+  // }
 
   //Image Upload
   $("#img-uploader").click(function (e) {
@@ -169,27 +169,27 @@ $(document).ready(function () {
 });
 
 //Banner background image slide show
-var folder = "";
-var pictures = [
-  "images/Rental.jpg",
-  "images/realty%20Finance.jpg",
-  "images/Water.jpg",
-  "images/Savings.jpg",
-];
-var num = 0;
+// var folder = "";
+// var pictures = [
+//   "images/Rental.jpg",
+//   "images/realty%20Finance.jpg",
+//   "images/Water.jpg",
+//   "images/Savings.jpg",
+// ];
+// var num = 0;
 
-function main() {
-  var slider = document.getElementById("section");
-  if (num == 3) num = 0;
-  else num++;
+// function main() {
+//   var slider = document.getElementById("section");
+//   if (num == 3) num = 0;
+//   else num++;
 
-  slider.style.backgroundImage =
-    "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(" +
-    pictures[num] +
-    ")";
-}
+//   slider.style.backgroundImage =
+//     "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(" +
+//     pictures[num] +
+//     ")";
+// }
 
-setInterval("main()", 3000);
+// setInterval("main()", 3000);
 
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
